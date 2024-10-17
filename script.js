6a4093f0f876b7d2a9ae574ab7dfa981f1f3d943
valdov <ualvin@gmail.com>
2024-10-16 21:37:04 -0700

    added delay to hidden text and color animaton in footer

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
+const wede_1 = document.getElementById("wede_1")
+const wede_2 = document.getElementById("wede_2")
+const wede_3 = document.getElementById("wede_3")
+const textElement = document.getElementById('animatedText');
+let hue = 0;
+
 document.addEventListener('contextmenu', event => {
     event.preventDefault();
 });
 document.addEventListener('mousedown', dragging => {
     dragging.preventDefault();
 });
+function showWede_1(){
+    sleep(100);
+    wede_1.style.display = "block";
+}
+function hideWede_1(){
+    sleep(100);
+    wede_1.style.display = "none";
+}
+function showWede_2(){
+    sleep(100);
+    wede_2.style.display = "block";
+}
+function hideWede_2(){
+    sleep(100);
+    wede_2.style.display = "none";
+}
+function showWede_3(){
+    sleep(100);
+    wede_3.style.display = "block";
+}
+function hideWede_3(){
+    sleep(100);
+    wede_3.style.display = "none";
+}
+function animateTextColor() {
+    hue = (hue + 1) % 720;
+    textElement.style.color = `hsl(${hue}, 100%, 50%)`;
+    requestAnimationFrame(animateTextColor);
+}
+animateTextColor();
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
